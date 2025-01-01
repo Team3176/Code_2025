31422f110da24a72db4f54bdc2a7075726323688
char@odin <char3176@gmail.com>
2024-12-31 22:58:52 -0500

    build test 1

--- src/main/java/team3176/robot/BuildConstants.java
+++ src/main/java/team3176/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package team3176.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Code_2024";
+  public static final String MAVEN_NAME = "Code_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 576;
-  public static final String GIT_SHA = "4d4d5a355b1a8b71582cc2d9d8c000e831c4fa85";
-  public static final String GIT_DATE = "2024-12-21 01:12:26 EST";
-  public static final String GIT_BRANCH = "MKPods_beta";
-  public static final String BUILD_DATE = "2024-12-21 01:44:35 EST";
-  public static final long BUILD_UNIX_TIME = 1734763475419L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "b10aa3ef849b794ebe407308df4d88c13ddc715a";
+  public static final String GIT_DATE = "2024-12-31 16:46:41 EST";
+  public static final String GIT_BRANCH = "prune2024mechs";
+  public static final String BUILD_DATE = "2024-12-31 18:46:42 EST";
+  public static final long BUILD_UNIX_TIME = 1735688802846L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
