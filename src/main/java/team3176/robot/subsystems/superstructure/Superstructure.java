281bd7ce9cbfd745731de0cbb4a9cd3d6f8d694f
Faith Opafola <faithopafola@gmail.com>
2025-01-23 20:49:23 -0500

    added command for operator climb

--- src/main/java/team3176/robot/subsystems/superstructure/Superstructure.java
+++ src/main/java/team3176/robot/subsystems/superstructure/Superstructure.java
@@ -1,20 +1,32 @@
 package team3176.robot.subsystems.superstructure;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import java.util.function.DoubleSupplier;
 import team3176.robot.FieldConstants;
 // import java.util.function.IntSupplier;
 import team3176.robot.subsystems.drivetrain.Drivetrain;
+import team3176.robot.subsystems.superstructure.climb.Climb;
 
 public class Superstructure {
   private static Superstructure instance;
+  private Climb climb;
 
-  public Superstructure() {}
+  public Superstructure() {
+    climb = Climb.getInstance();
+  }
 
   /*
   public Command climbDown() {
     return climb.moveLeftRightPosition(0, 0);
   }
   */
+  public Command moveClimbLeftPosition(DoubleSupplier position) {
+    return climb.moveLeftPosition(position);
+  }
+
+  public Command stopClimbLeft() {
+    return climb.stopLeft();
+  }
 
   public Command getProcessorCoralLeftAuto() {
     return Drivetrain.getInstance()
