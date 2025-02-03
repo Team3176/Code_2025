a2e0cab0adeb586c614c88ff9e696c4f3555bcc1
char@odin <char3176@gmail.com>
2025-02-02 22:54:53 -0500

    Check if mech exists in Superstructure before constructing it

--- src/main/java/team3176/robot/subsystems/superstructure/Superstructure.java
+++ src/main/java/team3176/robot/subsystems/superstructure/Superstructure.java
@@ -3,12 +3,60 @@ package team3176.robot.subsystems.superstructure;
 import edu.wpi.first.wpilibj2.command.Command;
 import team3176.robot.FieldConstants;
 // import java.util.function.IntSupplier;
+//import team3176.robot.subsystems.superstructure.arm.Arm;
+//import team3176.robot.subsystems.superstructure.climb.Climb;
 import team3176.robot.subsystems.drivetrain.Drivetrain;
+//import team3176.robot.subsystems.superstructure.elevator.Elevator;
+//import team3176.robot.subsystems.superstructure.intake.Intake;
 
 public class Superstructure {
   private static Superstructure instance;
+  //private Arm arm;
+  //private Climb climb;
+  private Drivetrain drivetrain;
+  //private Elevator elevator;
+  //private Intake intake;
 
-  public Superstructure() {}
+
+
+  public Superstructure() {
+    //Arm arm = null;
+    //Climb climb = null;
+    //Drivetrain drivetrain = null;
+    //Elevator elevator = null;
+    //Intake intake = null;
+
+    /* 
+    if (arm == null) {
+      arm = Arm.getInstance();
+    }
+    */
+
+    /*
+    if (climb == null) {
+      climb = Climb.getInstance();
+    } 
+    */
+
+    /* 
+    if (drivetrain == null) {
+      drivetrain = Drivetrain.getInstance();
+    }
+    */
+
+    /*
+    if (elevator == null) {
+      elevator = Elevator.getInstance();
+    }
+    */
+
+    /*
+    if (intake == null) {
+      intake = Intake.getInstance();
+    }
+    */ 
+
+  }
 
   /*
   public Command climbDown() {
