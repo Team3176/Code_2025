53827fa287e36251cd8043663e31437069e97315
char@odin <char3176@gmail.com>
2025-02-10 10:08:53 -0500

    adding ability to use Pigeon2

--- src/main/java/team3176/robot/subsystems/drivetrain/GyroIO.java
+++ src/main/java/team3176/robot/subsystems/drivetrain/GyroIO.java
@@ -19,6 +19,14 @@ public interface GyroIO {
     boolean isConnected = false;
     public double[] odometryYawTimestamps = new double[] {};
     public Rotation2d[] odometryYawPositions = new Rotation2d[] {};
+
+    /* 
+    public boolean connected = false;
+    public Rotation2d yawPosition = new Rotation2d();
+    public double yawVelocityRadPerSec = 0.0;
+    public double[] odometryYawTimestamps = new double[] {};
+    public Rotation2d[] odometryYawPositions = new Rotation2d[] {};
+    */
   }
 
   /** Updates the set of loggable inputs. */
--- src/main/java/team3176/robot/subsystems/drivetrain/GyroIONavX.java
+++ src/main/java/team3176/robot/subsystems/drivetrain/GyroIONavX.java
@@ -1,18 +1,42 @@
 package team3176.robot.subsystems.drivetrain;
 
 import com.kauailabs.navx.frc.AHRS;
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.SPI;
+import team3176.robot.constants.DriveConstants;
+
 import java.util.OptionalDouble;
 import java.util.Queue;
 
 public class GyroIONavX implements GyroIO {
   AHRS navX;
+  //private final Pigeon2 pigeon = new Pigeon2(5, "canivore");
+  //private final StatusSignal<Angle> yaw = pigeon.getYaw();
   private final Queue<Double> yawPositionQueue;
   private final Queue<Double> yawTimestampQueue;
+  //private final StatusSignal<AngularVelocity> yawVelocity = pigeon.getAngularVelocityZWorld();
+
 
   public GyroIONavX() {
     navX = new AHRS(SPI.Port.kMXP);
+
+    /* 
+    pigeon.getConfigurator().apply(new Pigeon2Configuration());
+    pigeon.getConfigurator().setYaw(0.0);
+    yaw.setUpdateFrequency(DriveConstants.odometryFrequency);
+    yawVelocity.setUpdateFrequency(50.0);
+    pigeon.optimizeBusUtilization();
+    yawTimestampQueue = PhoenixOdometryThread.getInstance().makeTimestampQueue();
+    yawPositionQueue = PhoenixOdometryThread.getInstance().registerSignal(pigeon.getYaw());
+    tryUntilOk(5, () -> pigeon.setYaw(0.0, 0.25));
+    */
 
     yawTimestampQueue = TalonOdometryThread.getInstance().makeTimestampQueue();
     yawPositionQueue =
@@ -26,6 +50,7 @@ public class GyroIONavX implements GyroIO {
                     return OptionalDouble.empty();
                   }
                 });
+    //yawPositionQueue = TalonOdometryThread.getInstance().registerSignal(pigeon.getYaw);
   }
 
   @Override
@@ -43,6 +68,20 @@ public class GyroIONavX implements GyroIO {
             .toArray(Rotation2d[]::new);
     yawTimestampQueue.clear();
     yawPositionQueue.clear();
+
+    /* 
+    inputs.connected = BaseStatusSignal.refreshAll(yaw, yawVelocity).equals(StatusCode.OK);
+    inputs.yawPosition = Rotation2d.fromDegrees(yaw.getValueAsDouble());
+    inputs.yawVelocityRadPerSec = Units.degreesToRadians(yawVelocity.getValueAsDouble());
+
+    inputs.odometryYawTimestamps =
+        yawTimestampQueue.stream().mapToDouble((Double value) -> value).toArray();
+    inputs.odometryYawPositions =
+        yawPositionQueue.stream().map(Rotation2d::fromDegrees).toArray(Rotation2d[]::new);
+    yawTimestampQueue.clear();
+    yawPositionQueue.clear();
+    */
+
   }
 
   @Override
