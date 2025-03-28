f8f0d635b901a4a05917f5e4c9d5fde74dbb006a
AAgraviador <teragon123@outlook.com>
2025-03-27 22:02:21 -0400

    Auto stash before merge of "akit-run3" and "origin/akit-run3"

--- src/main/java/com/team3176/robot/RobotContainer.java
+++ src/main/java/com/team3176/robot/RobotContainer.java
@@ -119,23 +119,22 @@ public class RobotContainer {
     // }
 
 
-/*     NamedCommands.registerCommand("L1", superstructure.goToL1()
-        .withDeadline(new WaitCommand(2.5).andThen(superstructure.shoot().withTimeout(1)))
-        .andThen(superstructure.stopRollers())
-        .andThen(superstructure.goToL0().withTimeout(2.5))); */
+
     NamedCommands.registerCommand("L2", superstructure.goToL2()
-        .withDeadline(new WaitCommand(2.5).andThen(superstructure.shoot().withTimeout(1)))
+        .withDeadline(new WaitCommand(1.5).andThen(superstructure.shoot().withTimeout(1)))
         .andThen(superstructure.stopRollers())
         .andThen(superstructure.goToL0().withTimeout(2.5)));
     NamedCommands.registerCommand("L3", superstructure.goToL3()
-        .withDeadline(new WaitCommand(2.5).andThen(superstructure.shoot().withTimeout(1)))
+        .withDeadline(new WaitCommand(1.5).andThen(superstructure.shoot().withTimeout(1)))
         .andThen(superstructure.stopRollers())
         .andThen(superstructure.goToL0().withTimeout(2.5)));
     // this keeps the elevator up while we shoot and then brings it down
     NamedCommands.registerCommand("L4", superstructure.goToL4()
-        .withDeadline(new WaitCommand(2.5).andThen(superstructure.shoot().withTimeout(1)))
+        .withDeadline(new WaitCommand(1.5).andThen(superstructure.shoot().withTimeout(1)))
         .andThen(superstructure.stopRollers())
         .andThen(superstructure.goToL0().withTimeout(2.5)));
+
+    //NamedCommands.registerCommand("L4Auto", superstructure.goToL4().andThen());
 
 /*     NamedCommands.registerCommand("L1", superstructure.goToL1().alongWith(Arm.getInstance().setPivotCurrents())
         .withDeadline(new WaitCommand(2.5).andThen(superstructure.deAlgaePositive().withTimeout(2)))
