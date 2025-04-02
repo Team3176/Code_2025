3eba40d6791d29eca3dfc632d3deb0c7bbbb9d6e
char@prometheus <char3176@gmail.com>
2025-04-02 18:34:19 -0400

    Changes from Tue night

--- src/main/java/com/team3176/robot/commands/AlignToReef.java
+++ src/main/java/com/team3176/robot/commands/AlignToReef.java
@@ -194,7 +194,7 @@ public class AlignToReef {
     private Pose2d getWaypointFromBranch(Pose2d branch){
         return new Pose2d(
             branch.getTranslation(),
-            branch.getRotation().rotateBy(Rotation2d.k180deg)
+            branch.getRotation().rotateBy(Rotation2d.kCCW_90deg)
         );
     }
 
